987cc645f5469da77d3b437fb4490a060f921c2b
CoderCatA5 <theayushsingh72@gmail.com>
2022-08-26 14:53:13 +0530

    templates

--- mapper_task_1.py
+++ mapper_task_1.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+import json
+import sys
+for line in sys.stdin:
+  try:
+    tweet=json.loads(line)
+    tokenset=[x.lower() for x in tweet['analysis']['tokens']['all']]
+    tokens=set(tokenset)
+    for token in tokens:
+      print json.dumps(token.lower()),'\t',1
+  except:
+    pass
--- reducer_task_1.py
+++ reducer_task_1.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import sys
+prevWord=""
+valueTotal=0
+for line in sys.stdin:
+  (word,values)=line.split('\t')
+  value=int(values.strip())
+  if word==prevWord or prevWord=='':
+    valueTotal=valueTotal+value
+    prevWord=word
+  else:
+    print prevWord,'\t',valueTotal
+    prevWord=word
+    valueTotal=value
+print prevWord,'\t',valueTotal
