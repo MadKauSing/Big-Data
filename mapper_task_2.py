377314416d4731a31284e59fce1b670b885d7a6d
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-08-26 22:16:47 +0530

    Added mapper and reducer for task 2

--- mapper_task_2.py
+++ mapper_task_2.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 import json
 import sys
+
+
+for line in sys.stdin:
+    try:
+        data = json.loads(line)
+        # print(data)
+
+        if((data["humidity"] > 48 and data["humidity"] < 54) and (data["temperature"] > 20 and data["temperature"] < 24)):
+            print(f"{data['timestamp']},{data['lat']},{data['lon']},1")
+    except:
+        pass
--- reducer_task_2.py
+++ reducer_task_2.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python3
 import sys
+import math
+
+dictionary = {}
+keys = []
+
+d = float(sys.argv[1])
+lat = float(sys.argv[2])
+lon = float(sys.argv[3])
+
+for line in sys.stdin:
+    # faster lookups
+    try:
+        line = line.strip()
+        # print(line)
+        word, data_lat, data_lon, values = line.rsplit(",")
+        # print(word,data_lat,data_lon,values)
+
+        value = int(values.strip())
+        dist = math.sqrt((lat - float(data_lat.strip()))**2 + (lon - float(data_lon.strip()))**2)
+        if dist < d:
+            if word not in dictionary.keys():
+                dictionary[word] = value
+            else:
+                dictionary[word] = dictionary[word]+value
+    except Exception as e:
+        print(e)
+for date in dictionary:
+  print(date,dictionary[date])
