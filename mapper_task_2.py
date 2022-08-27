d1bd0964be831aaf1a8168dbca965aace4d723da
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-08-27 10:04:06 +0530

    changed mapper for task 2

--- mapper_task_2.py
+++ mapper_task_2.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 import json
 import sys
+import math
+
+# command line arguments
+d = float(sys.argv[1])
+lat = float(sys.argv[2])
+lon = float(sys.argv[3])
+
+
+def check_constraints(data):
+    if data["humidity"] > 48 and data["humidity"] < 54:
+        if data["temperature"] > 20 and data["temperature"] < 24:
+            return 1
+    return 0
+
+
+def check_distance(data):
+    dist = math.sqrt((lat - data["lat"])**2 + (lon - data["lon"])**2)
+    if dist < d:
+        return 1
+    return 0
 
 
 for line in sys.stdin:
@@ -8,7 +28,8 @@ for line in sys.stdin:
         data = json.loads(line)
         # print(data)
 
-        if((data["humidity"] > 48 and data["humidity"] < 54) and (data["temperature"] > 20 and data["temperature"] < 24)):
-            print(f"{data['timestamp']},{data['lat']},{data['lon']},1")
+        if check_constraints(data):
+            if check_distance(data):
+                print(f"{data['timestamp']},1")
     except:
         pass
