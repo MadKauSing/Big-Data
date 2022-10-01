0edf7dec9e1107b2bab5dbc186e09fea7cf578d0
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-10-01 11:54:22 +0530

    Task 2 skeleton

--- A2/mapper1.py
+++ A2/mapper1.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import sys
-#do oone github pull
 print_function=print
 
 for line in sys.stdin:
     try:
-
         temp=line.split()
         if temp[0]!='#':
             source,dest=temp
---
+++ A2/task2final/reducer2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+
+def calculate_rank():
+    #Rank(p) = 0.34 + 0.57 ∑ Contribution of nodes pointing to p
+    
+    pass
