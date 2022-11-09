f1f398d1bd16b88cbc388070e187b7ec47e525dd
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-11-09 20:01:04 +0530

    Fixed comments and spacing

--- A3/Task1/Task1.py
+++ A3/Task1/Task1.py
@@ -18,22 +18,23 @@ df = spark.read.csv(sys.argv[1],header=True,inferSchema=True)
 # Drop all null valued rows
 df = df.na.drop()
 
-# Eliminate duplicates
+# Eliminate duplicate rows
 df = df.dropDuplicates()
 
+# Finding average of each state
 avg = df.groupby('RP State Plate').avg('Fine amount')
 avg.filter(avg['RP State Plate'] == 'WA').show()
-# Query condition
-res_1 = df.filter(df["Color"] == "WH")
 
+# Query conditions
+res_1 = df.filter(df["Color"] == "WH")
 res_join = res_1.join(avg,res_1["RP State Plate"] == avg["RP State Plate"], "inner")
-# res_join.show()
 res = res_join.filter(res_join["Fine amount"] > res_join["avg(Fine amount)"])
 res.filter(res["Ticket number"] == 1109149683).show()
+
 # Sort shuffled data
 res = res.sort("Ticket number")
-# res.show(10)
 
+# Select only the ticket number column
 res = res.select("Ticket number")
 
 # Delete ouput folder if it already exists
