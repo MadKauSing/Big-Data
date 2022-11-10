32e648b6079521b66785c0c8b7073be691405fb2
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-11-10 12:41:18 +0530

    computations done

--- A3/Task2/kafka-consumer.py
+++ A3/Task2/kafka-consumer.py
@@ -1,15 +1,23 @@
 from kafka import KafkaConsumer
 import sys
-from json import loads
+from json import dumps, loads
 
 tn = sys.argv[1]
-print(tn)
 consumer = KafkaConsumer(tn, bootstrap_servers=[
                          'localhost:9092'], value_deserializer=lambda x: loads(x.decode('utf-8')))
-
+main = dict()
 try:
     for message in consumer:
-        print("%s:%d:%d: key=%s value=%s" % (message.topic,
-              message.partition, message.offset, message.key, message.value))
+        msg = message.value
+        (key, value), = msg.items()
+        m = dict()
+        d = {"Min": value[0], "Max": value[1]}
+        m[key] = d
+        obj = dumps(m)
+        print(obj)
+        # print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
+    # print(main)
+    # obj=json.dumps(main)
+    # print(obj)
 except KeyboardInterrupt:
     sys.exit()
--- A3/Task2/kafka-producer.py
+++ A3/Task2/kafka-producer.py
@@ -2,19 +2,31 @@ import sys
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import csv
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 tn = sys.argv[1]
+main = dict()
 # for _ in range(100):
 #    producer.send('foobar', b'some_message_bytes')
 for line in sys.stdin:
     # print(file)
-    l = line.strip().split(',')
+    l = list(csv.reader([line]))[0]
+    # print(l)
     if l[0] != "EOF":
-        d = {l[0]: [l[6], l[7]]}
-        ack = producer.send(tn, value=d)
-        meta = ack.get()
-        print(meta.topic)
-        print(meta.partition)
-        print("sent to consumer")
+        d = [float(l[6]), float(l[7]), 1]
+        if l[0] not in main.keys():
+            main[l[0]] = d
+        else:
+            count = main[l[0]][2]
+            new_val = [round((((main[l[0]][0]*count)+d[0])/(count+1)), 2),
+                       round((((main[l[0]][1]*count)+d[1])/(count+1)), 2), count+1]
+            main.update({l[0]: new_val})
+for key in sorted(main.keys()):
+    ack = producer.send(tn, value={key: [main[key][0], main[key][1]]})
+    meta = ack.get()
+# for key,value in main.items():
+#    new_val=[value[0],value[1]]
+#    ack=producer.send(tn,value={key:new_val})
+#    meta=ack.get()
