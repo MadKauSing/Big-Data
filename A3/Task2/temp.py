fb9a8e16ae184e2a656e8077c6c19866887a2c71
AyushmaanKaushik <ayushmaan1362@gmail.com>
2022-11-10 15:09:03 +0530

    Finished task 2

--- A3/Task2/temp.py
+++ A3/Task2/temp.py
@@ -6,14 +6,25 @@ tn = sys.argv[1]
 consumer = KafkaConsumer(tn, bootstrap_servers=[
                          'localhost:9092'], value_deserializer=lambda x: loads(x.decode('utf-8')))
 main = dict()
+print("{")
 
 for message in consumer:
     msg = message.value
     (key, value), = msg.items()
     m = dict()
-    if(msg=={"msg":"End of transmission"}):
-        break
     d = {"Min": value[0], "Max": value[1]}
-    main[key] = d    
+    main[key] = d
+    print("""
+{
+"%s" : {"min" : %f,"max" : %f}
+},
+""" % (key,value[0],value[1]),end="")
+    # obj = dumps(m)
+    # print(obj)
+    # print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
+# print(main)
 obj=dumps(main)
 print(obj)
+# print(obj)
+
+print("}")
