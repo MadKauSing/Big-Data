eb5e30f5a57e30be41caed7f4ac603d772dd1bd0
BhaviniMadhuranath <bhaviniphillippe@gmail.com>
2022-11-10 10:01:23 +0530

    :task 2 sending msg

--- A3/Task2/kafka-consumer.py
+++ A3/Task2/kafka-consumer.py
@@ -1,5 +1,15 @@
 from kafka import KafkaConsumer
+import sys
+from json import loads
 
-consumer = KafkaConsumer('my_favorite_topic')
-for msg in consumer:
-    print (msg)
+tn = sys.argv[1]
+print(tn)
+consumer = KafkaConsumer(tn, bootstrap_servers=[
+                         'localhost:9092'], value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+try:
+    for message in consumer:
+        print("%s:%d:%d: key=%s value=%s" % (message.topic,
+              message.partition, message.offset, message.key, message.value))
+except KeyboardInterrupt:
+    sys.exit()
--- A3/Task2/kafka-producer.py
+++ A3/Task2/kafka-producer.py
@@ -1,5 +1,20 @@
+import sys
+from time import sleep
+from json import dumps
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers='localhost:1234')
-for _ in range(100):
-    producer.send('foobar', b'some_message_bytes')
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+tn = sys.argv[1]
+# for _ in range(100):
+#    producer.send('foobar', b'some_message_bytes')
+for line in sys.stdin:
+    # print(file)
+    l = line.strip().split(',')
+    if l[0] != "EOF":
+        d = {l[0]: [l[6], l[7]]}
+        ack = producer.send(tn, value=d)
+        meta = ack.get()
+        print(meta.topic)
+        print(meta.partition)
+        print("sent to consumer")
